0f4ca4acaf48ec6ed03f7eeff9df079f4e70595d
DESKTOP-9PTFMOR\ybaba <jmachang368@gmail.com>
2023-02-11 23:00:25 +0900

    dev-toolsを使えるようにした

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,5 +1,4 @@
 import { render } from 'solid-js/web';
-
 import { TodoList } from './todo-list';
 
 const root = document.getElementById('root');
--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    devtools({ autoname: true }),
+    solidPlugin()
+  ],
   server: {
     port: 3000,
   },
